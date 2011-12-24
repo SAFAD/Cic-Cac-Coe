35fcc2f234630e006f59417a8970d47a5a34372f
Sadaoui Abderrahim <SAFAD.Line@gmail.com>
2011-12-24 14:55:09 +0100

    Version 1.1

--- cic-cac-coe.cpp
+++ cic-cac-coe.cpp
@@ -1,66 +1,83 @@
-###################################################
-##################Cic-Cac-Coe######################
-######## All rights reserved to SAFAD(C) ##########
-###########Please read the LICENSE file############
-###################################################
+/*###################################################
+  ##################Cic-Cac-Coe######################
+  ######## All rights reserved to SAFAD(C) ##########
+  ###########Please read the LICENSE file############
+  ###################################################*/
 
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
 //X or O ?
 char XorO[9] = {'0','1','2','3','4','5','6','7','8'};
 //the game is running or not ? 1 yes , 0 no
-int game_status = 1;
-//rounds 1 = player1, 2 = player2
+int game_status = 0;
+//rounds 1 = player1, 2 = player2 OR CPU
 int rounds = 1;
 //to define the character we put in the column
 char block;
 //the user input
 int selection;
+//AI vs Human
+int opponent;
 
 //show winner !
 void show_winner(){
 	cout << "Winner is Player " << rounds << " congratulations !!" << endl;
 }
 
-//check who won !
-bool check_winner(){
-	if(XorO[0] && XorO[1] && XorO[2] == 'X' || XorO[0] && XorO[1] && XorO[2] == 'O'){
-		return true;
-	}
-	if(XorO[3] && XorO[4] && XorO[5] == 'X' || XorO[3] && XorO[4] && XorO[5] == 'O'){
-		return true;
-	}
-	if(XorO[6] && XorO[7] && XorO[8] == 'X' || XorO[6] && XorO[7] && XorO[8] == 'O'){
-		return true;
-	}
-	if(XorO[0] && XorO[3] && XorO[6] == 'X' || XorO[0] && XorO[3] && XorO[6] == 'O'){
-		return true;
-	}
-	if(XorO[1] && XorO[4] && XorO[7] == 'X' || XorO[1] && XorO[4] && XorO[7] == 'O'){
-		return true;
-	}
-	if(XorO[2] && XorO[5] && XorO[8] == 'X' || XorO[2] && XorO[5] && XorO[8] == 'O'){
-		return true;
-	}
-	if(XorO[0] && XorO[4] && XorO[8] == 'X' || XorO[0] && XorO[4] && XorO[8] == 'O'){
-		return true;
-	}
-	if(XorO[2] && XorO[4] && XorO[6] == 'X' || XorO[2] && XorO[4] && XorO[6] == 'O'){
-		return true;
-	}
-	
-	return false;
+//show the board
+void show_board() {
+    cout << "+++++++++++++++++++++++++++++" << endl;
+    for(int i=0; i<7; i+=3) {
+        cout << "+++++ "<< XorO[i] <<" ++++ "<< XorO[i+1] << " +++++ " << XorO[i+2] <<" ++++++" << endl;
+        cout << "+++++++++++++++++++++++++++++" << endl;
+    }
+}
+//tell them spike, tell them its a draw !!
+void show_draw(){
+	cout << "Nothing to see here Peeps, its a Draw" << endl;
 }
 
-//function to switch from player one to two and vice versa
-void switch_turns(){
-	if(rounds == 1){
-		rounds = 2;
-	}
-	else{
-		rounds = 1;
-	}
+//check who won !
+bool check_winner(){
+    
+    for(int i=0; i<7; i+=3) {
+        if((XorO[i] == 'X' && XorO[i+1] == 'X' && XorO[i+2] == 'X') || (XorO[i] == 'O' && XorO[i+1] == 'O' && XorO[i+2] == 'O')){
+            return true;
+        }
+    }
+    
+    for(int i=0; i<3; i++) {
+        if((XorO[i] == 'X' && XorO[i+3] == 'X' && XorO[i+6] == 'X') || (XorO[i] == 'O' && XorO[i+3] == 'O' && XorO[i+6] == 'O')){
+            return true;
+        }
+    }
+    
+    for(int i=0; i<3; i+=2) {
+        if((XorO[i] == 'X' && XorO[4] == 'X' && XorO[8-i] == 'X') || (XorO[i] == 'O' && XorO[4] == 'O' && XorO[8-i] == 'O')){
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool check_draw(){
+	if( XorO[0] != '0' &&
+		XorO[1] != '1' &&
+		XorO[2] != '2' &&
+		XorO[3] != '3' &&
+		XorO[4] != '4' &&
+		XorO[5] != '5' &&
+		XorO[6] != '6' &&
+		XorO[7] != '7' &&
+		XorO[8] != '8'
+	   ){
+	   	return true;
+	   }
+	return false;
+    
 }
 
 //validates the user input
@@ -73,40 +90,76 @@ bool validate_selection(int selection){
 	}
 }
 
+//function to switch from player one to two and vice versa
+void switch_turns(){
+	if(rounds == 1){
+		rounds = 2;
+	}	
+	else{
+		rounds = 1;
+	}
+}
+
+void check_game_status(){
+	//before we switch turns, we check if the user won or not
+	if(check_winner()){
+		show_board();
+		show_winner();
+		game_status = 0;
+	}
+	//what if the game is a Draw ?
+	else if(check_draw()){
+		show_board();
+		show_draw();
+		game_status = 0;
+	
+	}
+	else{
+		switch_turns();
+	}
+}
+
+
+
 //function to show the board
 void board(){
-	cout << "+++++++++++++++++++++++++++++" << endl;
-	cout << "+++++ "<< XorO[0] <<" ++++ "<< XorO[1] << " +++++ " << XorO[2] <<" ++++++" << endl;
-	cout << "+++++++++++++++++++++++++++++" << endl;
-	cout << "+++++ "<< XorO[3] <<" ++++ "<< XorO[4] << " +++++ " << XorO[5] <<" ++++++" << endl;
-	cout << "+++++++++++++++++++++++++++++" << endl;
-	cout << "+++++ "<< XorO[6] <<" ++++ "<< XorO[7] << " +++++ " << XorO[8] <<" ++++++" << endl;
-	cout << "+++++++++++++++++++++++++++++" << endl;
+	show_board();
 	if(rounds == 1){
 		block = 'X';
 	}
 	else{
 		block = 'O';
 	}
-	cout << "Player " << rounds << " Please enter a valid number (0-8) :" <<endl;
-	cin >> selection;
-	if(validate_selection(selection)){
-		XorO[selection] = block;
-		//before we switch turns, we check if the user won or not
-		if(!check_winner()){
-			switch_turns();
+	//Artificial Inteligence :)
+	if(opponent == 2 && rounds == 2){
+		selection = rand() % 9;
+		if(validate_selection(selection)){
+			XorO[selection] = block;
+			check_game_status();
 		}
-		else{
-			show_winner();
-			game_status = 0;
+	}
+	else{
+		cout << "Player " << rounds << " Please enter a valid number (0-8) :" <<endl;
+		cin >> selection;
+		if(validate_selection(selection)){
+			XorO[selection] = block;
+			check_game_status();
 		}
 	}
 	
 }
 
 int main(){
+	if(game_status == 0){
+		cout << "Play VS :" << endl;
+		cout << "1. Human" << endl;
+		cout << "2. AI (CPU)" << endl;
+		cin >> opponent;
+		game_status = 1;
+	}
 	while(game_status == 1){
 		board();
 	}
+	
 }
 
