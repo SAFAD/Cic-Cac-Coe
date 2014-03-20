87ff5dafd6d11f05a205a6e6e04186db8cb39eac
Sadaoui Abderrahim <SAFAD.Line@gmail.com>
2014-03-20 15:51:28 +0100

    Implemented MinMax, it feels a bit bad, but I will work it out

--- cic-cac-coe.cpp
+++ cic-cac-coe.cpp
@@ -15,8 +15,8 @@ using namespace std;
 vector<int> originalBoard;
 //the game is running or not ? 1 yes , 0 no
 int status = 0;
-//turns 1 = player1, 2 = player2 OR CPU
-char turn = 1;
+//turns 1 = player1, -1 = player2 OR CPU
+char turn = -1;
 //to define the character we put in the column
 char block;
 //the user input
@@ -29,11 +29,11 @@ void run();
 //show the board
 char ReplaceBlocks(int blockId){
 	switch(blockId) {
-        case 1:
+        case -1:
             return 'X'; //it is the first player..
         case 0:
             return ' '; //an empty block
-        case 2:
+        case 1:
             return 'O'; //second player here..
     }
     return ' ';
@@ -59,31 +59,31 @@ void show_winner(){
 }
 
 //check who won !
-bool check_winner(vector<int> board){
+int check_winner(vector<int> board){
 
     for(int i=0; i<7; i+=3) {
-        if((board[i] == 2 && board[i+1] == 2 && board[i+2] == 2) || (board[i] == 1 && board[i+1] == 1 && board[i+2] == 1)){
-            return true;
+        if((board[i] == -1 && board[i+1] == -1 && board[i+2] == -1) || (board[i] == 1 && board[i+1] == 1 && board[i+2] == 1)){
+            return board[i];
         }
     }
 
     for(int i=0; i<3; i++) {
-        if((board[i] == 2 && board[i+3] == 2 && board[i+6] == 2) || (board[i] == 1 && board[i+3] == 1 && board[i+6] == 1)){
-            return true;
+        if((board[i] == -1 && board[i+3] == -1 && board[i+6] == -1) || (board[i] == 1 && board[i+3] == 1 && board[i+6] == 1)){
+            return board[i];
         }
     }
 
     for(int i=0; i<3; i+=2) {
-        if((board[i] == 2 && board[4] == 2 && board[8-i] == 2) || (board[i] == 1 && board[4] == 1 && board[8-i] == 1)){
-            return true;
+        if((board[i] == -1 && board[4] == -1 && board[8-i] == -1) || (board[i] == 1 && board[4] == 1 && board[8-i] == 1)){
+            return board[i];
         }
     }
 
-    return false;
+    return 0;
 }
 
 bool check_draw(vector<int> board){
-    if (std::count(board.begin(), board.end(), '0') < 0)
+    if (std::count(board.begin(), board.end(), 0) < 0)
     {
         //there still are no more moves
         //now lets see if someone one
@@ -103,7 +103,7 @@ bool validate_selection(int selection){
 	{
 		return false;
 	}
-	if(originalBoard[selection] == 1 || originalBoard[selection] == 2){
+	if(originalBoard[selection] == 1 || originalBoard[selection] == -1){
 		return false;
 	}
 	else {
@@ -113,7 +113,7 @@ bool validate_selection(int selection){
 
 //function to switch from player one to two and vice versa
 void switch_turns(){
-	turn = (turn == 1) ? 2 : 1;
+	turn = (turn == 1) ? -1 : 1;
 	run();
 }
 
@@ -122,7 +122,6 @@ void check_status(vector<int> board){
 	//before we switch turns, we check if the user won or not
 	if(gameStatus == 0){
 		switch_turns();
-		
 	}
 	//what if the game is a Draw ?
 	else if(gameStatus == 2){
@@ -140,7 +139,7 @@ vector<int> getAllPossibleMoves(vector<int> board){
 	vector<int> spaces;
 	for (int i = 0; i < 9; ++i)
 	{
-		if (board[i] == '0')
+		if (board[i] == 0)
 		{
 			//this is the chosen one
 			spaces.push_back(i);
@@ -164,129 +163,53 @@ int checkGame(vector<int> board){
 	return 0;
 }
 
-int getScore(vector<int> board, char turn){
-	int bestScore = 0,
-		iterations = 0,
-		MAXWIN = 100,
-		penalty = -5;
-	//here, we read all the possible moves, and choose the best based on the score
-	//score, MAXWIN = 100, DRAW = 0, MAXLOSE = -5
-	//The MAXWIN score is acheived by doing the least of iterations
-	//MAXLOSE score is achieved by doing the most of iterations, this means the computer, if he is going to lose, choose the longest path
-	//How to calculate score: MAXWIN += -5*(iterations); MAXLOSE += 5*(iterations);
-	//first we get all the possible moves
-	vector<int> moves = getAllPossibleMoves(board);
-	//now we loop through every possibility
-	for(std::vector<int>::size_type i = 0; i != moves.size(); i++) 
+int minmax(vector<int> board, int player){
+	//player == 1 => X, == -1 => O
+	int win = check_winner(board),
+		bestScore = -2,
+		bestMove = -1;
+	if (win > 0) return player*win;
+
+	for(std::vector<int>::size_type i = 0; i != originalBoard.size(); i++) 
 	{
-		//okay so for this we clone the board, add our move
-		vector<int> newBoard(board);
-		//then we add a move 
-		newBoard[moves[i]] = turn;
-
-		//now we check if the game is won or a draw
-		if (checkGame(newBoard) == 1){
-			//this move, wins the game, but who won?
-			//if the AI wins : calculate the score, if it is better, save it as best score
-			//if the opponent wins : we calculate the lose score, compare it if it is better, then save it..
-			int score;
-			if (turn == 2)
+		if (board[i] == 0)
+		{
+			board[i] = player;
+			int score = -minmax(board, -player);
+			cout << score << endl;
+			if (score > bestScore)
 			{
-				//its me mittens!
-				score =  MAXWIN + penalty*iterations;
-			}else{
-				//its the human being...
-				score = -MAXWIN + penalty*iterations;
-				
+				bestScore = score;
+				bestMove = i;
 			}
-			bestScore = (bestScore < score) ? score : bestScore;
-		}else if (checkGame(newBoard) == 2){
-			//its a draw, we see if the bestScore is a losing score, we rather draw than lose..
-			bestScore = (bestScore < 0) ? 0 : bestScore;
-		}else{
-			//the game is unfinished
-			++iterations;
-			//now we create a new board, and get its best score..
-			vector<int> clonedBoard(newBoard);
-			char newTurn = (turn == 1) ? 2 : 1;
-			int score = getScore(clonedBoard,newTurn);
-
-			bestScore = (bestScore < score) ? score : bestScore;
+			board[i] = 0;
 		}
-
 	}
+	if(bestMove == -1) return 0;
 	return bestScore;
-}
-bool opponentMove(vector<int> board){
-	bool win = false;
-	vector<int> opponentMoves = getAllPossibleMoves(board);
-	for(std::vector<int>::size_type i = 0; i != opponentMoves.size(); i++) {
-		vector<int> newBoard(originalBoard);
-		newBoard[opponentMoves[i]] = 1;
-		if (checkGame(newBoard) == 1)
-		{
-			win = true;
-			break;
-		}
-	}
-	return win;
-}
-int nextMove(){
-	//first we see, if the game has just begun
-	//first we generate a list of all possible moves
-	//then we loop over it over each choice, if the game can be lost on the next human move
-	//priority = 0 if draw, 1 if lose, 2 if win;
-	int bestScore = 0,
-		bestPriority,
-		bestMove = 0;
-
-	vector<int> moves = getAllPossibleMoves(originalBoard);
-	int size = moves.size();
-
-	if (size == 8)
-	{
-		int random = rand() % moves[7] + moves[0];
-		return moves[random];
-
-	}
-	//now we see what the opponent would pick..
-	if (opponentMove(originalBoard))
-	{
-		/* code */
-	}
-	for(std::vector<int>::size_type i = 0; i != size; i++) {
-	    vector<int> newBoard(originalBoard);
-		newBoard[moves[i]] = 2;
-		//now we check whether the move wins us or not
-		int priority = checkGame(newBoard);
-		if (priority > bestPriority && priority != 0)
-		{
-			bestPriority = priority;
-			bestMove = moves[i];
-		}
-		else if (priority == 0)
-		{
-			//the game is unfinished, if true it means he can win
-			if (!opponentMove(newBoard) && priority > bestPriority)
-			{
-				bestPriority = priority;
-				bestMove = moves[i];
-			}
-		}
-	}
-	return bestMove;
 }
 void AI(){
 	//cout << "Calculating..." << endl;
-	int next = nextMove();
+	int bestScore = -2,
+		bestMove = -1;
+	for(std::vector<int>::size_type i = 0; i != originalBoard.size(); i++) {
+	    if (originalBoard[i] == 0)
+		{
+			originalBoard[i] = 1;
+			int score = -minmax(originalBoard, -1);
+			originalBoard[i] = 0;
+			if (score > bestScore)
+			{
+				bestScore = score;
+				bestMove = i;
+			}
+		}
+	}
+
 	//now we use it on screen
-	if(validate_selection(next)){
-		originalBoard[next] = 2;
-		check_status(originalBoard);
-	}
-	else{
-		AI();
-	}
+	originalBoard[bestMove] = 1;
+	check_status(originalBoard);
+	
 }
 
 void player(){
@@ -304,9 +227,18 @@ void player(){
 
 //function to show the board
 void run(){
-	DrawBoard();
+	if (opponent == 2)
+	{
+		if (turn == -1)
+		{
+			DrawBoard();
+		}
+		
+	}else{
+		DrawBoard();
+	}
 	//Artificial Inteligence :)
-	if(opponent == 2 && turn == 2){
+	if(opponent == 2 && turn == 1){
 		AI();
 	}
 	else{
@@ -317,7 +249,7 @@ void run(){
 
 int main(){
 	//we first populate the board with dummy strings
-	char dummy[9] = {'0','0','0','0','0','0','0','0','0'};
+	int dummy[9] = {0,0,0,0,0,0,0,0,0};
 
 	for(int index=0;index<9; ++index)
 	{
